4a0d6ae1a32e946384ad3b959aa727f415e49398
Clinton <manakanejustin@gmail.com>
2022-10-18 11:17:43 +0200

    updated cart to show link to shop when it is empty

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import Layout from "../components/Layout";
 import CartCard from "../components/DisplayCards/cartcard";
+import { Link } from "react-router-dom";
 
 function Cart() {
   const items = useSelector((state) => state.cart.cart);
@@ -27,28 +28,38 @@ function Cart() {
       </header>
 
       <section className="">
-        <div className="container sm:grid grid-cols-3">
-          <div className="sm:col-span-2">
-            {items.map((item, index) => (
-              <CartCard product={item} key={index} />
-            ))}
+        {!sum && (
+          <div className="flex h-96 space-y-4 flex-col justify-center items-center">
+            <p>You have no items in cart.</p>
+            <Link className="border-2 border-black px-4 py-1" to="/items">
+              Go shopping
+            </Link>
           </div>
-
-          <div className="sm:col-span-1 h-max p-2">
-            <header className="py-2 border-b-2 border-black">
-              <h2 className="text-xl">Summary</h2>
-            </header>
-            <div className="py-4 gap-4 grid grid-cols-2">
-              <p>Subtotal</p>
-              <p>R {sum}</p>
-              <p>Shipping</p>
-              <p>R 0.00</p>
+        )}
+        {sum > 0 && (
+          <div className="container sm:grid grid-cols-3">
+            <div className="sm:col-span-2">
+              {items.map((item, index) => (
+                <CartCard product={item} key={index} />
+              ))}
             </div>
-            <button className="w-full px-4 py-1 text-base font-medium rounded bg-blue-500 text-white">
-              Checkout
-            </button>
+
+            <div className="sm:col-span-1 h-max p-2">
+              <header className="py-2 border-b-2 border-black">
+                <h2 className="text-xl">Summary</h2>
+              </header>
+              <div className="py-4 gap-4 grid grid-cols-2">
+                <p>Subtotal</p>
+                <p>R {sum}</p>
+                <p>Shipping</p>
+                <p>R 0.00</p>
+              </div>
+              <button className="w-full px-4 py-1 text-base font-medium rounded bg-blue-500 text-white">
+                Checkout
+              </button>
+            </div>
           </div>
-        </div>
+        )}
       </section>
     </Layout>
   );
